fe2d0e4f4d30e148289596f706092bd909edee3e
Isaac Shabtay <isaac-s@users.noreply.github.com>
2020-06-04 13:39:00 -0700

    Removed older API which is apparently not needed anymore

--- src/main/java/co/cloudify/jenkins/plugin/CloudifyBuildStep.java
+++ src/main/java/co/cloudify/jenkins/plugin/CloudifyBuildStep.java
@@ -1,7 +1,6 @@
 package co.cloudify.jenkins.plugin;
 
 import java.io.IOException;
-import java.util.Arrays;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.builder.ToStringBuilder;
@@ -16,8 +15,6 @@ import hudson.FilePath;
 import hudson.Launcher;
 import hudson.model.AbstractBuild;
 import hudson.model.BuildListener;
-import hudson.model.Cause;
-import hudson.model.Result;
 import hudson.model.Run;
 import hudson.model.TaskListener;
 import hudson.tasks.Builder;
@@ -108,33 +105,6 @@ public abstract class CloudifyBuildStep extends Builder implements SimpleBuildSt
         } catch (Exception ex) {
             ex.printStackTrace(listener.getLogger());
             throw new AbortException("Failed performing step");
-        }
-    }
-
-    /**
-     * Generic implementation of a Cloudify build step: perform some boilerplate
-     * code before and after the main implementation.
-     */
-    @Override
-    public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener)
-            throws InterruptedException, IOException {
-        listener.started(Arrays.asList(new Cause.UserIdCause()));
-        CloudifyClient client = getCloudifyClient(build);
-        EnvVars envVars = CloudifyPluginUtilities.getEnvironment(build, listener);
-
-        try {
-            performImpl(build, launcher, listener, build.getWorkspace(), envVars, client);
-            listener.finished(Result.SUCCESS);
-            return true;
-        } catch (IOException | InterruptedException ex) {
-            // Print to the build log, as Jenkins doesn't do this.
-            // This is good for diagnostics.
-            ex.printStackTrace(listener.getLogger());
-            throw ex;
-        } catch (Exception ex) {
-            ex.printStackTrace(listener.getLogger());
-            listener.finished(Result.FAILURE);
-            throw new AbortException(String.format("Unexpected exception was raised: [%s]", ex));
         }
     }
 
